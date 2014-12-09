5e28a4da5b27bb5d081b3d4ddeeda2abada05a04
Ray Jenkins <rjenkins@aceevo.com>
2014-12-09 15:50:36 -0600

    cleanup unused imports

--- src/main/java/com/librato/disco/DiscoService.java
+++ src/main/java/com/librato/disco/DiscoService.java
@@ -9,8 +9,6 @@ import org.apache.zookeeper.CreateMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 /**
  * Handle starting/stopping of the zookeeper client (framework) and creation of ephemeral node for service discovery
  */
