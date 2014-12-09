bf666858b5cf80024b5174c09990785413e7a9d1
Ray Jenkins <rjenkins@aceevo.com>
2014-12-09 14:23:24 -0600

    changing implementation to require host/port on start

--- src/main/java/com/librato/disco/DiscoService.java
+++ src/main/java/com/librato/disco/DiscoService.java
@@ -17,30 +17,26 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class DiscoService {
     private static final Logger log = LoggerFactory.getLogger(DiscoService.class);
     private static final String baseNodeTemplate = "/services/%s/nodes";
-    private String baseNode;
-    private CuratorFramework framework;
-    private String nodeName;
-    private int port;
-    private String node;
-    private ConnectionStateListener listener;
+    private final String baseNode;
+    CuratorFramework framework;
+    String nodeName;
+    int port;
+    String node;
+    ConnectionStateListener listener;
 
-    private DiscoService() {
-        // for use with DynamicDiscoServiceWrapper
+    public DiscoService(CuratorFramework framework, String serviceName) {
+        this.framework = framework;
+        this.baseNode = String.format(baseNodeTemplate, serviceName);
     }
 
-    public DiscoService(CuratorFramework framework, String serviceName, String nodeName, int port) {
-        this.framework = framework;
-        this.baseNode = getBaseNode(serviceName);
+    public void start(String nodeName, int port) throws Exception {
+        Preconditions.checkArgument(framework.getState() == CuratorFrameworkState.STARTED);
         this.nodeName = nodeName;
         this.port = port;
-        init();
-    }
 
-    private void init() {
         // Register ephemeral node as representation of this service's nodename and port
         // such as /services/myservice/nodes/192.168.1.1:8000
         this.node = baseNode + "/" + nodeName + ":" + port;
-
         this.listener = new ConnectionStateListener() {
             @Override
             public void stateChanged(CuratorFramework curatorFramework, ConnectionState connectionState) {
@@ -57,10 +53,7 @@ public class DiscoService {
                 }
             }
         };
-    }
 
-    public void start() throws Exception {
-        Preconditions.checkArgument(framework.getState() == CuratorFrameworkState.STARTED);
         // Ensure the parent paths exist persistently
         while (framework.checkExists().forPath(baseNode) == null) {
             log.info("Creating base node {}", baseNode);
@@ -91,61 +84,6 @@ public class DiscoService {
     private void deleteNode() throws Exception {
         if (framework.checkExists().forPath(node) != null) {
             framework.delete().forPath(node);
-        }
-    }
-
-    private String getBaseNode(String serviceName) {
-        return String.format(baseNodeTemplate, serviceName);
-    }
-
-
-    public static DynamicDiscoServiceWrapper DynamicDiscoServiceWrapper() {
-        return new DynamicDiscoServiceWrapper();
-    }
-
-    /**
-     * An static builder and adapter for DiscoService that allows the user to
-     * provide parameters using fluent with statements as data is available
-     */
-    public static class DynamicDiscoServiceWrapper {
-
-        private final DiscoService discoService = new DiscoService();
-        private AtomicBoolean initCalled = new AtomicBoolean(false);
-
-        public DynamicDiscoServiceWrapper withFramework(CuratorFramework curatorFramework) {
-            discoService.framework = curatorFramework;
-            return this;
-        }
-
-        public DynamicDiscoServiceWrapper withServiceName(String serviceName) {
-            discoService.baseNode = String.format(baseNodeTemplate, serviceName);
-            return this;
-        }
-
-        public DynamicDiscoServiceWrapper withNodeName(String nodeName) {
-            discoService.nodeName = nodeName;
-            return this;
-        }
-
-        public DynamicDiscoServiceWrapper withPort(int port) {
-            discoService.port = port;
-            return this;
-        }
-
-        public void init() {
-            discoService.init();
-            initCalled.set(true);
-        }
-
-        public void start() throws Exception {
-            if(initCalled.get() == false) {
-                throw new RuntimeException("init must be called prior to calling start()");
-            }
-            discoService.start();
-        }
-
-        public void stop() throws Exception {
-            discoService.stop();
         }
     }
 }
--- src/test/java/com/librato/disco/DiscoServiceTest.java
+++ src/test/java/com/librato/disco/DiscoServiceTest.java
@@ -7,7 +7,6 @@ import org.apache.curator.framework.api.DeleteBuilder;
 import org.apache.curator.framework.api.ExistsBuilder;
 import org.apache.curator.framework.imps.CuratorFrameworkState;
 import org.apache.curator.framework.listen.Listenable;
-import org.apache.curator.framework.state.ConnectionStateListener;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
@@ -27,64 +26,30 @@ public class DiscoServiceTest {
         when(framework.getState()).thenReturn(CuratorFrameworkState.STARTED);
         ACLBackgroundPathAndBytesable<String> os = mock(ACLBackgroundPathAndBytesable.class);
         when(createBuilder.withMode(CreateMode.EPHEMERAL)).thenReturn(os);
-        DiscoService service = new DiscoService(framework, "myservice", "foo", 4321);
-        service.start();
+        DiscoService service = new DiscoService(framework, "myservice") ;
+        service.start("foo", 4321);
         verify(os).forPath("/services/myservice/nodes/foo:4321");
     }
 
     @Test
     public void testStop() throws Exception {
+        Stat stat = new Stat();
         String path = "/services/myservice/nodes/foo:1234";
         CuratorFramework framework = mockFramework();
         ExistsBuilder existsBuilder = mock(ExistsBuilder.class);
-        when(existsBuilder.forPath(path)).thenReturn(mock(Stat.class));
+        when(existsBuilder.forPath(path)).thenReturn(stat);
         when(framework.checkExists()).thenReturn(existsBuilder);
         DeleteBuilder deleteBuilder = mock(DeleteBuilder.class);
         when(framework.delete()).thenReturn(deleteBuilder);
-        DiscoService manager = new DiscoService(framework, "myservice", "foo", 1234);
+        DiscoService manager = new DiscoService(framework, "myservice");
+        manager.node = path;
         manager.stop();
-        verify(deleteBuilder).forPath(path);
-    }
-
-    @Test
-    public void testDynamicDiscoServiceWrapperStart() throws Exception {
-        CuratorFramework framework = mockFramework();
-        ExistsBuilder ceBuilder = mock(ExistsBuilder.class);
-        CreateBuilder createBuilder = mock(CreateBuilder.class);
-        when(framework.checkExists()).thenReturn(ceBuilder);
-        when(ceBuilder.forPath("/services/myservice/nodes")).thenReturn(mock(Stat.class));
-        when(framework.create()).thenReturn(createBuilder);
-        when(framework.getState()).thenReturn(CuratorFrameworkState.STARTED);
-        ACLBackgroundPathAndBytesable<String> os = mock(ACLBackgroundPathAndBytesable.class);
-        when(createBuilder.withMode(CreateMode.EPHEMERAL)).thenReturn(os);
-        DiscoService.DynamicDiscoServiceWrapper dynamicDiscoServiceWrapper = new DiscoService.DynamicDiscoServiceWrapper()
-                .withFramework(framework).withServiceName("myservice").withNodeName("foo").withPort(7777);
-        dynamicDiscoServiceWrapper.init();
-        dynamicDiscoServiceWrapper.start();
-        verify(os).forPath("/services/myservice/nodes/foo:7777");
-    }
-
-    @Test
-    public void testDynamicDiscoServiceWrapperStop() throws Exception {
-        String path = "/services/myservice/nodes/foo:6666";
-        CuratorFramework framework = mockFramework();
-        ExistsBuilder existsBuilder = mock(ExistsBuilder.class);
-        when(existsBuilder.forPath(path)).thenReturn(mock(Stat.class));
-        when(framework.checkExists()).thenReturn(existsBuilder);
-        DeleteBuilder deleteBuilder = mock(DeleteBuilder.class);
-        when(framework.delete()).thenReturn(deleteBuilder);
-        DiscoService.DynamicDiscoServiceWrapper dynamicDiscoServiceWrapper = new DiscoService.DynamicDiscoServiceWrapper()
-                .withFramework(framework).withServiceName("myservice").withNodeName("foo").withPort(6666);
-        dynamicDiscoServiceWrapper.init();
-        dynamicDiscoServiceWrapper.stop();
         verify(deleteBuilder).forPath(path);
     }
 
     private CuratorFramework mockFramework() {
         CuratorFramework framework = mock(CuratorFramework.class);
-
         when(framework.getConnectionStateListenable()).thenReturn(mock(Listenable.class));
-
         return framework;
     }
 }
